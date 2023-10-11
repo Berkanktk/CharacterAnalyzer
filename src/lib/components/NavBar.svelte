32123f704043568a5cd6627e1395fb33cdb057f4
Berkanktk <berkank@live.dk>
2023-10-11 04:35:46 +0200

    Added navbar

---
+++ src/lib/components/NavBar.svelte
@@ -0,0 +1,7 @@
+<div class="navbar w-full border-b border-base-content/10">
+    <div class="navbar-start"></div>
+    <div class="navbar-center my-4">
+        <h1 class="font-bold text-4xl">Character Analyzer</h1>
+    </div>
+    <div class="navbar-end"></div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
     import "../app.css";
+    import NavBar from '$lib/components/NavBar.svelte';
 </script>
 
 <div class="min-h-screen flex flex-col">
+    <NavBar />
     <slot />
 </div>
