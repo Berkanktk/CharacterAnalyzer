f59d08ff305c15a77e6586b19a5d63d38e01dac9
Berkanktk <berkank@live.dk>
2023-10-11 21:19:08 +0200

    Added theme switch and updated navbar

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -1,7 +1,45 @@
+<script lang="ts">
+    import { theme } from '../../routes/stores'
+    import { onMount } from 'svelte'
+
+    let localTheme: string;
+
+  // Function to toggle the theme
+  function toggleTheme() {
+    theme.update((currentTheme) =>
+      currentTheme === "light" ? "dark" : "light"
+      );
+  }
+
+    // Set the theme in localStorage and update the HTML attribute on theme change
+    onMount(() => {
+    theme.subscribe((newTheme) => {
+      localStorage.setItem("theme", newTheme);
+      document.documentElement.setAttribute("data-theme", newTheme);
+    });
+  });
+</script>
+
 <div class="navbar w-full border-b border-base-content/10">
-    <div class="navbar-start"></div>
-    <div class="navbar-center my-4">
-        <h1 class="font-bold text-4xl">Character Analyzer</h1>
+    <div class="navbar-start">
+      <a href="/about" class="btn btn-ghost">Explanations</a>
+    
+      <details class="dropdown">
+        <summary class="m-1 btn btn-ghost"><img src="language.svg" alt="language">Language</summary>
+        <ul class="p-2 shadow menu dropdown-content z-1 bg-base-100 rounded-box w-52">
+          <li><a>English</a></li>
+          <li><a>Danish</a></li>
+          <li><a>Turkish</a></li>
+        </ul>
+      </details>
     </div>
-    <div class="navbar-end"></div>
-</div>
+    <div class="navbar-center my-4">
+      <h1 class="font-bold text-4xl">Character Analyzer</h1>
+    </div>
+    <div class="navbar-end">
+      <a href="https://github.com/Berkanktk/CharacterAnalyzer" class="btn btn-ghost" target="_blank"><img src="github.svg" alt="github"></a>
+      <button class="btn btn-ghost ml-2" on:click={toggleTheme}>
+        <img src={localTheme === "light" ? "sun.svg" : "moon.svg"} alt={localTheme === "light" ? "sun" : "moon"} />
+      </button>
+    </div>
+  </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,4 +5,31 @@ export default {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark"]
+    // [
+    //   {'dark': {
+    //     "primary": "#793ef9",
+    //     "primary-focus": "#570df8",
+    //     "primary-content": "#ffffff",
+    //     "secondary": "#f000b8",
+    //     "secondary-focus": "#bd0091",
+    //     "secondary-content": "#ffffff",
+    //     "accent": "#37cdbe",
+    //     "accent-focus": "#2aa79b",
+    //     "accent-content": "#ffffff",
+    //     "neutral": "#2a2e37",
+    //     "neutral-focus": "#16181d",
+    //     "neutral-content": "#ffffff",
+    //     "base-100": "#191e24",
+    //     "base-200": "#2a2e37",
+    //     "base-300": "#16181d",
+    //     "base-content": "#ebecf0",
+    //     "info": "#66c6ff",
+    //     "success": "#87d039",
+    //     "warning": "#e2d562",
+    //     "error": "#ff6f6f"
+    //   }},
+    // ],
+  },
 }
