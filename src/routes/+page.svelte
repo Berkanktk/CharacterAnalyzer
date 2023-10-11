e205e655ad36468626cc36ab6d3bc538c1be8a96
Berkanktk <berkank@live.dk>
2023-10-11 04:36:28 +0200

    Added the first look

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,90 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex">
+    <div class="w-1/4 p-10 text-center mt-20">
+        <div class="stats stats-vertical shadow">
+            <div class="stat place-items-center">
+                <div class="stat-title">Characters (without space)</div>
+                <div class="stat-value">0</div>
+            </div>
+
+            <div class="stat place-items-center">
+                <div class="stat-title">Unique Words</div>
+                <div class="stat-value">0</div>
+            </div>
+
+            <div class="stat place-items-center">
+                <div class="stat-title">Reading Time</div>
+                <div class="stat-value">0</div>
+            </div>
+
+            <div class="stat place-items-center">
+                <div class="stat-title">Speech Time</div>
+                <div class="stat-value">0</div>
+            </div>
+
+            <div class="stat place-items-center">
+                <div class="stat-title">Average Word Length</div>
+                <div class="stat-value">0</div>
+            </div>
+        </div>
+    </div>
+
+    <div class="mx-auto">
+        <div class="w-full">
+            <div class="stats shadow mx-auto mt-10 w-full">
+                <div class="stat place-items-center">
+                    <div class="stat-figure text-secondary" />
+                    <div class="stat-title">Characters</div>
+                    <div class="stat-value">0</div>
+                </div>
+
+                <div class="stat place-items-center">
+                    <div class="stat-figure text-secondary" />
+                    <div class="stat-title">Words</div>
+                    <div class="stat-value">0</div>
+                </div>
+
+                <div class="stat place-items-center">
+                    <div class="stat-figure text-secondary" />
+                    <div class="stat-title">Paragraphs</div>
+                    <div class="stat-value">0</div>
+                </div>
+
+                <div class="stat place-items-center">
+                    <div class="stat-figure text-secondary" />
+                    <div class="stat-title">Sentences</div>
+                    <div class="stat-value">0</div>
+                </div>
+            </div>
+
+            <div class="form-control mt-10 w-full">
+                <textarea
+                    class="textarea textarea-bordered textarea-sm w-full"
+                    placeholder="Enter text here"
+                    rows="10"
+                />
+            </div>
+
+            <div class="mt-10">
+                <button class="btn btn-primary">UPPERCASE</button>
+                <button class="btn btn-primary">lowercase</button>
+                <button class="btn btn-primary">Capitalized Case</button>
+                <button class="btn btn-primary">Sentence case</button>
+                <button class="btn btn-primary">iNVERT cASE</button>
+                <button class="btn btn-primary">rAnDoM</button>
+                <button class="btn btn-primary">Reverse</button>
+                <button class="btn btn-primary">Flip</button>
+            </div>
+        </div>
+    </div>
+
+    <div class="w-1/4 p-10">
+        <h1 class="text-3xl font-bold text-center">Frequency</h1>
+        <p>Nothing yet</p>
+    </div>
+</div>
+
+<style>
+    .btn {
+        text-transform: none;
+    }
+</style>
