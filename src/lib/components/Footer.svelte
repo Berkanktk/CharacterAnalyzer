b4513f62506a1b05d7d6726448ecae8b305f43d4
Berkanktk <berkank@live.dk>
2023-10-11 19:03:54 +0200

    Minor mobile opt

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,6 +1,6 @@
-<footer class="w-full">
+<footer class="w-full text-center">
     <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://berkankutuk.dk/" class="hover:underline">Berkan Kütük</a>. All Rights Reserved.</span>
+        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://berkankutuk.dk/" target="_blank" class="hover:underline">Berkan Kütük</a>. All Rights Reserved.</span>
     </div>
 </footer>
