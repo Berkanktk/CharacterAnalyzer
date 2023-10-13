8b55ed48659a9f37a3d9ed2b9e2ad878bcb3e25c
Berkanktk <berkank@live.dk>
2023-10-13 03:51:32 +0200

    Added an about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,54 @@
+<main class="container mx-auto p-4">
+  <section class="my-8">
+    <h1 class="text-4xl font-bold">About the Application</h1>
+    <p class="mt-4 text-lg">
+      Say hello to a feature-rich character analysis application providing several tools and features to help you analyze and manipulate text in various ways.
+    </p>
+  </section>
+
+  <section class="my-8">
+    <h2 class="text-2xl font-semibold">Word Analysis</h2>
+    <p class="mt-4 text-lg">The application allows you to analyze text in many ways, including:</p>
+    <ul class="list-disc pl-6 mt-2">
+      <li>Counting characters, words, paragraphs and sentences</li>
+      <li>Calculating reading and speech time.</li>
+      <li>Determining the average word length.</li>
+      <li>Calculating the total amount of unique words.</li>
+    </ul>
+  </section>
+
+  <section class="my-8">
+    <h2 class="text-2xl font-semibold">Text Transformation</h2>
+    <p class="mt-4 text-lg">You can also transform text with ease:</p>
+    <ul class="list-disc pl-6 mt-2">
+      <li>Convert text to uppercase or lowercase.</li>
+      <li>Capitalize text in various ways.</li>
+      <li>Invert the case of characters.</li>
+      <li>Create random capitalization.</li>
+      <li>Reverse the text order.</li>
+    </ul>
+  </section>
+
+  <section class="my-8">
+    <h2 class="text-2xl font-semibold">Word Frequency</h2>
+    <p class="mt-4 text-lg">The application provides insights into word frequency and usage.</p>
+  </section>
+
+  <section class="my-8">
+    <h2 class="text-2xl font-semibold">Search and Replace</h2>
+    <p class="mt-4 text-lg">You can search and replace text by opening the search and replace tool.</p>
+  </section>
+
+  <section class="my-8">
+    <h2 class="text-2xl font-semibold">Accessibility</h2>
+    <p class="mt-4 text-lg">The application is fully accessible and supports:</p>
+    <ul class="list-disc pl-6 mt-2">
+      <li>Keyboard navigation.</li>
+      <li>Screen readers.</li>
+      <li>Light mode.</li>
+      <li>Dark mode.</li>
+      <li>Internationalization through various languages.</li>
+    </ul>
+  </section>
+</main>
+  
