a97fd02ff3e58c02c28d4be0cb849680ee05a4a5
Berkanktk <berkank@live.dk>
2023-10-13 04:12:24 +0200

    Support for languages in about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,53 +1,66 @@
+<script lang="ts">
+ import lang from '$lib/locales/all.json';
+ import { locale, localeData } from "../../routes/stores";
+
+ let data: any = {};
+ let currentLocale: App.LocaleKey;
+
+
+  // Subscribe to the locale store and update data accordingly
+  locale.subscribe((value: string) => {
+      currentLocale = value as App.LocaleKey; 
+      
+      if (currentLocale in lang) {
+          data = lang[currentLocale];
+          localeData.set(data);
+      }
+  });
+</script>
+
 <main class="container mx-auto p-4">
   <section class="my-8">
-    <h1 class="text-4xl font-bold">About the Application</h1>
-    <p class="mt-4 text-lg">
-      Say hello to a feature-rich character analysis application providing several tools and features to help you analyze and manipulate text in various ways.
-    </p>
+    <h1 class="text-4xl font-bold">{data.aboutTitle}</h1>
+    <p class="mt-4 text-lg">{data.aboutContent}</p>
   </section>
 
   <section class="my-8">
-    <h2 class="text-2xl font-semibold">Word Analysis</h2>
-    <p class="mt-4 text-lg">The application allows you to analyze text in many ways, including:</p>
+    <h2 class="text-2xl font-semibold">{data.wordAnalysisTitle}</h2>
+    <p class="mt-4 text-lg">{data.wordAnalysisContent}</p>
     <ul class="list-disc pl-6 mt-2">
-      <li>Counting characters, words, paragraphs and sentences</li>
-      <li>Calculating reading and speech time.</li>
-      <li>Determining the average word length.</li>
-      <li>Calculating the total amount of unique words.</li>
+      {#each lang[currentLocale].wordAnalysisList as item (item)}
+        <li>{item}</li>
+      {/each}
+    </ul>
+    
+  </section>
+
+  <section class="my-8">
+    <h2 class="text-2xl font-semibold">{data.textTransformationTitle}</h2>
+    <p class="mt-4 text-lg">{data.textTransformationContent}</p>
+    <ul class="list-disc pl-6 mt-2">
+      {#each lang[currentLocale].textTransformationList as item (item)}
+        <li>{item}</li>
+      {/each}
     </ul>
   </section>
 
   <section class="my-8">
-    <h2 class="text-2xl font-semibold">Text Transformation</h2>
-    <p class="mt-4 text-lg">You can also transform text with ease:</p>
+    <h2 class="text-2xl font-semibold">{data.wordFrequencyTitle}</h2>
+    <p class="mt-4 text-lg">{data.wordFrequencyContent}</p>
+  </section>
+
+  <section class="my-8">
+    <h2 class="text-2xl font-semibold">{data.searchReplaceTitle}</h2>
+    <p class="mt-4 text-lg">{data.searchReplaceContent}</p>
+  </section>
+
+  <section class="my-8">
+    <h2 class="text-2xl font-semibold">{data.accessibilityTitle}</h2>
+    <p class="mt-4 text-lg">{data.accessibilityContent}</p>
     <ul class="list-disc pl-6 mt-2">
-      <li>Convert text to uppercase or lowercase.</li>
-      <li>Capitalize text in various ways.</li>
-      <li>Invert the case of characters.</li>
-      <li>Create random capitalization.</li>
-      <li>Reverse the text order.</li>
-    </ul>
-  </section>
-
-  <section class="my-8">
-    <h2 class="text-2xl font-semibold">Word Frequency</h2>
-    <p class="mt-4 text-lg">The application provides insights into word frequency and usage.</p>
-  </section>
-
-  <section class="my-8">
-    <h2 class="text-2xl font-semibold">Search and Replace</h2>
-    <p class="mt-4 text-lg">You can search and replace text by opening the search and replace tool.</p>
-  </section>
-
-  <section class="my-8">
-    <h2 class="text-2xl font-semibold">Accessibility</h2>
-    <p class="mt-4 text-lg">The application is fully accessible and supports:</p>
-    <ul class="list-disc pl-6 mt-2">
-      <li>Keyboard navigation.</li>
-      <li>Screen readers.</li>
-      <li>Light mode.</li>
-      <li>Dark mode.</li>
-      <li>Internationalization through various languages.</li>
+      {#each lang[currentLocale].accessibilityList as item (item)}
+        <li>{item}</li>
+      {/each}
     </ul>
   </section>
 </main>
